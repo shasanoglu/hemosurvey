8414dfbe479daa0ad3479dbd396979a0dc76cb89
Mehmet Sinan Hasanoglu <m.s.hasanoglu@gmail.com>
2016-01-02 20:46:47 +0200

    Creating new hasta is implemented

--- hasta/views.py
+++ hasta/views.py
@@ -9,6 +9,14 @@ def add_hasta(request):
         hastaForm = HastaForm(request.POST)
         kateterOlayiForm = KateterOlayiForm(request.POST,prefix='kof')
         if hastaForm.is_valid() and  kateterOlayiForm.is_valid():
+            hasta = hastaForm.save(commit=False)
+            hasta.merkez = request.user.profil.merkez
+            hasta.save()
+            kateterOlayi = kateterOlayiForm.save(commit=False)
+            kateterOlayi.hasta = hasta
+            kateterOlayi.save()
             return redirect('home') #TASK: Change this to hastalar view
 
-    return render(request,'hasta/add_hasta.html',{'hastaForm':hastaForm,'kateterOlayiForm':kateterOlayiForm})
+    context = {'hastaForm':hastaForm,'kateterOlayiForm':kateterOlayiForm}
+    return render(request,'hasta/add_hasta.html', context)
+
